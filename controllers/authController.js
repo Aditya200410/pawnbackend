6e8369b4981bd98516560a1e7da4506153853b23
Aditya200410 <adityakumar098k@gmail.com>
2025-06-27 12:09:42 +0530

    build fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const TempUser = require('../models/TempUser');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
