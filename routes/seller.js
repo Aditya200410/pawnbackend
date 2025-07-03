2f363f359ccf677f500c977aa5e8303c776d587d
Aditya200410 <adityakumar098k@gmail.com>
2025-07-03 21:59:38 +0530

    revert

--- routes/seller.js
+++ routes/seller.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const sellerAuthController = require('../controllers/sellerAuthController');
 const { handleMultipleImages, handleProfileImage } = require('../middleware/sellerUpload');
-const sellerAuth = require('../middleware/sellerAuth');
 
 // Test route
 router.get('/test', sellerAuthController.test);
@@ -17,9 +16,9 @@ router.post('/login', sellerAuthController.login);
 // Admin route to get all sellers
 router.get('/all', sellerAuthController.getAllSellers);
 
-// Profile routes (using JWT-based authentication)
-router.get('/profile', sellerAuth, sellerAuthController.getProfileByJWT);
-router.put('/profile', sellerAuth, sellerAuthController.updateProfileByJWT);
+// Profile routes (using email-based authentication)
+router.get('/profile', sellerAuthController.getProfile);
+router.put('/profile', sellerAuthController.updateProfile);
 router.post('/upload-images', handleMultipleImages, sellerAuthController.uploadImages);
 router.post('/upload-profile-image', handleProfileImage, sellerAuthController.uploadProfileImage);
 router.delete('/delete-image/:imageId', sellerAuthController.deleteImage);
