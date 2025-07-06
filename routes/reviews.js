b380a38aacf08f4ad384a78d666009926933c020
Aditya200410 <adityakumar098k@gmail.com>
2025-07-06 17:21:36 +0530

    email

--- routes/reviews.js
+++ routes/reviews.js
@@ -14,8 +14,22 @@ router.get('/product/:productId', getProductReviews);
 router.get('/user/:productId', getUserReview);
 
 // Routes that support both authenticated and unauthenticated users
-router.post('/', createReview); // Authentication optional
-router.put('/:reviewId', updateReview); // Authentication optional
-router.delete('/:reviewId', deleteReview); // Authentication optional
+// We'll create a custom middleware that makes authentication optional
+const optionalAuth = (req, res, next) => {
+  // Try to authenticate, but don't fail if no token
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    // If token is provided, try to authenticate
+    authenticateToken(req, res, next);
+  } else {
+    // No token provided, continue without authentication
+    req.user = null;
+    next();
+  }
+};
+
+router.post('/', optionalAuth, createReview);
+router.put('/:reviewId', optionalAuth, updateReview);
+router.delete('/:reviewId', optionalAuth, deleteReview);
 
 module.exports = router; 
