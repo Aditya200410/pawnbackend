aedd551edf1135d552d01e17f7d2c7c4b305bd9d
Aditya200410 <adityakumar098k@gmail.com>
2025-07-08 22:45:27 +0530

    fix

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ app.use((err, req, res, next) => {
 });
 
 // Port from environment variable
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
     
