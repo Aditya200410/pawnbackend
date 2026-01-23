e8ab35a7c9b19fbb9483e663f6acdc72393f4065
Aditya200410 <adityakumar098k@gmail.com>
2026-01-23 15:36:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,8 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 
 // Ensure data directories exist
